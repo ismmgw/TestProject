5c66709f5c646c2288bcc01c3d2f8c3c9ff1912c
maximus <ismmgw@gmail.com>
2016-08-05 10:22:44 +0800

    jquery add

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$( function() {
+    $( "#slider" ).slider({
+        value:1000,
+        min: 500,
+        max: 4999,
+        step: 1,
+        slide: function( event, ui ) {
+            $( "#amount" ).val( ui.value );
+            $( "#amount_count" ).val( ui.value=parseInt(ui.value*0.08) +" P" );
+        }
+    });
+    $( "#amount" ).val(  $( "#slider" ).slider( "value" ) );
+    $( "#amount_count" ).val(  $( "#slider" ).slider( "value" )*0.08+" P" );
+} );
+
+$( function() {
+    $( "#slider2" ).slider({
+        value:8000,
+        min: 5000,
+        max: 9999,
+        step: 1,
+        slide: function( event, ui ) {
+            $( "#amount2" ).val( ui.value );
+            $( "#amount_count2" ).val( ui.value=parseInt(ui.value*0.2) +" P" );
+        }
+    });
+    $( "#amount2" ).val(  $( "#slider2" ).slider( "value" ) );
+    $( "#amount_count2" ).val(  $( "#slider2" ).slider( "value" )*0.2+" P" );
+} );
+
+$( function() {
+    $( "#slider3" ).slider({
+        value:14000,
+        min: 10000,
+        max: 15000,
+        step: 1,
+        slide: function( event, ui ) {
+            $( "#amount3" ).val( ui.value );
+            $( "#amount_count3" ).val( ui.value=parseInt(ui.value*0.32) +" P" );
+        }
+    });
+    $( "#amount3" ).val(  $( "#slider3" ).slider( "value" ) );
+    $( "#amount_count3" ).val(  $( "#slider3" ).slider( "value" )*0.32+" P" );
+} );
